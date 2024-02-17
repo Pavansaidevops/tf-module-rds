26b72c9eb867205e6f384b8c5b092c6f070a9f58
Pavan Sai <pavansaidevops7@gmail.com>
2024-02-17 13:10:51 +0530

    terraform rds

---
+++ data.tf
@@ -0,0 +1,11 @@
+data "aws_ssm_parameter" "master_username" {
+  name = "docdb.${var.env}.master_username"
+}
+
+data "aws_ssm_parameter" "master_password" {
+  name = "docdb.${var.env}.master_password"
+}
+
+data "aws_ssm_parameter" "database_name" {
+  name = "rds.${var.env}.database_name"
+}
